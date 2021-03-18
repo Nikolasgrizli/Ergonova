de6278a9c9a617c6b7b7001b3f15822a5a45f102
Евсюков Николай Михайлович <nikolasgrizli@gmail.com>
2021-03-18 21:12:31 +0200

    tab on index

--- static/src/js/index.js
+++ static/src/js/index.js
@@ -212,7 +212,33 @@ if ($(window).width() < 1024) {
         }
     }
 
+/// tab on index
+// let tabs = document.querySelector('.tabs')
+// let btns = tabs.querySelectorAll('.tabs__btn')
+// let items = tabs.querySelectorAll('.tabs__item')
+
+// function change(arr, i) {
+
+//     arr.forEach( item => {
+
+//         item.forEach( elem => {alert(elem); elem.classList.remove('is-active')})
+//         item[i].classList.add('is-active')
+//     })
+// }
+
+// for(let i = 0; i < btns.length; i++) {
+//     btns[i].addEventListener('click', () => {
+//         change([btns, items], i)
+//     })
+// }
+
+
+
 })();
+
+
+
+
 
 
 //send form
